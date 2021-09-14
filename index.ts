ecacf0ebac0b859fcf31eb7c10a1708b61ba278c
Pavel Kuznetosv <kuznetsov@cmsplanet.ru>
2021-09-14 15:13:28 +0300

    Add ‘del’ function

--- index.ts
+++ index.ts
@@ -9,7 +9,27 @@ const url = process.env.APP_URL;
 
 const bot = new Telegraf(token);
 
+if (url)
+    bot.telegram.setWebhook(url);
+
 bot.start((ctx) => ctx.reply('Hello World'));
+
+bot.command('del', ctx => 
+{
+    if (ctx.from.username != 'diegov')
+        return;
+
+    if (ctx.message.reply_to_message != null)
+    {
+        ctx.telegram.deleteMessage(ctx.chat.id, ctx.message.reply_to_message.message_id);
+        ctx.telegram.deleteMessage(ctx.chat.id, ctx.message.message_id);
+    }
+})
+
+bot.hears('!check', ctx => 
+{
+    ctx.reply("I am alive!", { reply_to_message_id : ctx.message.message_id })
+})
 
 bot.launch();
 
