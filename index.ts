f6c7d855d937ed777a0706e3281b7fded3059c4c
Pavel Kuznetosv <kuznetsov@cmsplanet.ru>
2021-09-15 17:09:39 +0300

    try fix heroku

--- index.ts
+++ index.ts
@@ -5,8 +5,19 @@ const { Schema } = mongoose;
 
 process.env["NTBA_FIX_319"] = "1";
 
-var http = require('http');
-http.createServer().listen(process.env.PORT || 6000)
+var express = require('express');
+var app     = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+//For avoidong Heroku $PORT error
+app.get('/', (request:any, response:any) => {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
+
 
 const token: string =
   process.env.BOT_TOKEN || require("./config.json").bot_token;
